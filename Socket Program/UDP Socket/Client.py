65e1cdf9d8dfca61972524868b64b3a970e3017e
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-07 22:51:42 +0800

    Mengganti Kesalahan Penulisan

--- Socket Program/UDP Socket/Client.py
+++ Socket Program/UDP Socket/Client.py
@@ -1,10 +1,16 @@
 ###import module socket
 import socket
 
-###buat pesan yang akan dikirimkan ke server dengan encode UTF-8
-UDP_Massage = bytes('halo dunia', 'UTF-8')
+###looping perintah
+while True:
+    ###input masukkan
+    UDP_Massage = input('masukkan kata = ')
 
-###buat UDP Stream yang digunakan untuk menangkap komunikasi dari server
-UDP_Client_Socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDP_Client_Socket.sendto(UDP_Massage, ('127.0.0.1', 6500))
+    ###conver dari string menjadi byte
+    UDP_Convert = bytes(UDP_Massage, 'UTF-8')
 
+    ###memulai sesi stram UDP
+    UDP_Stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    ###membentu komunikasi dengan server
+    UDP_Stream.sendto(UDP_Convert, ('127.0.0.1', 6500))
