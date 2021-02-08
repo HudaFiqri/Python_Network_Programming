c693e97c2dffa74e565014ce73d486a782a70b81
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2021-02-08 21:12:59 +0800

    Perbaikan Kesalahan Upload File

--- Socket Program/TCP Socket/Client.py
+++ Socket Program/TCP Socket/Client.py
@@ -1,16 +1,31 @@
-###import module socket
+###import modul yang akan digunakan
 import socket
+import os
 
-###looping perintah
+'''
+karena os yang digunakan pembuat adalah linux/ubuntu maka untuk membersihkan layar terminal
+perintahnya adalah [ clear ].
+dan untuk os yang menggunakan windows bisa menggunakan [ cls ].
+'''
+
+###clear terminal screen
+os.system('clear')
+print('TCP Client\n\n')
+
+###looping
 while True:
-    ###input masukkan
-    UDP_Massage = input('masukkan kata = ')
 
-    ###conver dari string menjadi byte
-    UDP_Convert = bytes(UDP_Massage, 'UTF-8')
+    ###input masukan
+    TCP_Input = input('masukkan kata = ')
 
-    ###memulai sesi stram UDP
-    UDP_Stream = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ###konvert string menjadi byte
+    TCP_Convert = bytes(TCP_Input, 'UTF-8')
 
-    ###membentu komunikasi dengan server
-    UDP_Stream.sendto(UDP_Convert, ('127.0.0.1', 6500))
+    ###membuat modul untuk TCP stream
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    ###membuat koneksi ke server
+    s.connect(('127.0.0.1', 6500))
+
+    ###mengirimkan data ke server
+    s.sendall(TCP_Convert)
